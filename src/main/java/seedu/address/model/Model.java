3406df689824887c0a74311329cf78beaa274f4e
Eric Yang Jia Wei <ericyjw@gmail.com>
2018-10-17 01:48:06 +0800

    Update Model.java

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -113,6 +113,7 @@ public interface Model {
      */
     void updateCca(Cca target, Cca editedCca);
   
+    /**
      * Replaces the given persons {@code target} with {@code editedPerson}.
      * {@code target} must exist in the address book.
      * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
